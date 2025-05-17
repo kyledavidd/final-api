40f93f18242a2fda370295a57c0d4594e52cecfa
kyledavidd <davidyhuankyled@gmail.com>
2025-05-17 15:09:59 +0800

    new commit

--- src/main/java/com/david/post/WebConfig.java
+++ src/main/java/com/david/post/WebConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
-    @Value("${ALLOWED_ORIGINS:http://localhost:5173}")
+    @Value("${ALLOWED_ORIGINS:https://final-api-sn9c.onrender.com,https://final-ui-ten.vercel.app}")
     private String allowedOrigins;
 
     @Bean
